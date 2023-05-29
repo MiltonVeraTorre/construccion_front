2977ff0457ced905268517030a20e22063113d0b
MiltonVera <a00833471@tec.mx>
2023-05-29 13:02:06 -0600

    legacy

---
+++ src/pages/index_legacy.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import MainLayout from '@/Layout/MainLayout'
+import ProgresoUsuario from '@/components/Index_legacy/ProgresoUsuario'
+import UltimosCursos from '@/components/Index_legacy/UltimosCursos'
+import Clasificacion from '@/components/Index_legacy/Clasificacion'
+import Recomendaciones from '@/components/Index_legacy/Recomendaciones'
+import Resumen from '@/components/Index_legacy/Resumen'
+
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <div className='md:grid md:grid-cols-2'>
+
+      <ProgresoUsuario />
+      <UltimosCursos />
+      <Clasificacion />
+      <Resumen />
+      <Recomendaciones />
+      </div>
+    </MainLayout>
+  )
+}
